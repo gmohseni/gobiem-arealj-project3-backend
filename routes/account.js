c435f10070c702c76d32b4ea3cfea3af3637a752
Areal Jones <jones.ar@husky.neu.edu>
2021-04-21 12:10:05 -0700

    trying to fix errors

--- routes/account.js
+++ routes/account.js
@@ -2,10 +2,11 @@ const express = require('express');
 
 // getAccount--> signin
 // createAccount --> signup
-import {signin, signup} from '../controllers/account.js'; 
+import {signin, signup, fetchUsers} from '../controllers/account.js'; 
 const router = express.Router();
 
 router.post('/signin', signin);
 router.post('/signup', signup);
+router.get('/', fetchUsers);
    
 export default router;
